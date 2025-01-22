2b2b52711a376155e2dff5b66be0fceff9c91f52
Mário Henrique <mariohenriquelp@gmail.com>
2025-01-22 18:19:16 -0300

    começando a refatorar para mvc

--- src/app.ts
+++ src/app.ts
@@ -2,16 +2,9 @@ import express from 'express';
 import * as mysql from 'mysql2/promise';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
+import { createConnection } from './database';
 
-function createConnection() {
-    return mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: 'root',
-        database: 'tickets',
-        port: 33060
-    });
-}
+
 const app = express();
 
 app.use(express.json());
@@ -65,24 +58,7 @@ app.get('/', (req, res) => {
     res.json('Hello World');
 });
 
-app.post('/auth/login', async (req, res) => {
-    const { email, password } = req.body;
-    const connection = await createConnection();
-    try {
-        const [rows] = await connection.execute<mysql.RowDataPacket[]>
-            ('SELECT * FROM user WHERE email = ?', [email]);
-        const user = rows.length ? rows[0] : null;
-        if (user && bcrypt.compareSync(password, user.password)) {
-            const token = jwt.sign({ id: user.id, email: user.email }, "123456", { expiresIn: "1h" })
 
-            res.json({ token });
-        } else {
-            res.status(401).json({ message: 'Invalid credentials' });
-        }
-    } finally {
-        connection.end();
-    }
-})
 
 app.post('/partners/register', async (req, res) => {
     const { name, email, password, company_name } = req.body;
---
+++ src/controller/auth-controller.ts
@@ -0,0 +1,27 @@
+import { Router } from "express";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import * as mysql from 'mysql2/promise';
+import { createConnection } from "../database";
+
+
+export const authroutes = Router();
+
+authroutes.post('/auth/login', async (req, res) => {
+    const { email, password } = req.body;
+    const connection = await createConnection();
+    try {
+        const [rows] = await connection.execute<mysql.RowDataPacket[]>
+            ('SELECT * FROM user WHERE email = ?', [email]);
+        const user = rows.length ? rows[0] : null;
+        if (user && bcrypt.compareSync(password, user.password)) {
+            const token = jwt.sign({ id: user.id, email: user.email }, "123456", { expiresIn: "1h" })
+
+            res.json({ token });
+        } else {
+            res.status(401).json({ message: 'Invalid credentials' });
+        }
+    } finally {
+        connection.end();
+    }
+})
